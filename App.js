c8cae9a496dda1cb002ddca81c43cb12a981f6cb
Ankit <a.vishnubhotla@ufl.edu>
2018-10-20 19:28:06 -0400

    Disregard Changes

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { MapView } from "expo";
-export default class App extends React.Component {
+import { createDrawerNavigator } from 'react-navigation'
+
+import { createBottomTabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export class Home extends React.Component {
   render() {
     return (
       <MapView
@@ -9,12 +14,59 @@ export default class App extends React.Component {
           flex: 1
         }}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: 33.74825,
+          longitude: -84.3880,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
         }}
       />
-    );
+  )};
+}
+
+export class Leaderboard extends React.Component {
+  render() {
+    return (
+      <Text> "Waz up" </Text>
+    )
+  };
+}
+
+export class Settings extends React.Component {
+  render() {
+    return (
+      <Text> "Waz up" </Text>
+    )
   }
 }
+
+export class createBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name = "ios-home" color = {tintColor} size = {24} />
+      )
+    }
+  },
+  Leaderboard: Leaderboard,
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name = "ios-settings" color = {tintColor} size = {24} />
+      )
+    }
+  }
+}, {
+  navigationOptions: {
+    tabBarVisible: true
+  },
+  tabBarOptions: {
+    activeTintColor: 'blue',
+    inactiveTintColor: 'grey'
+  }
+})
+
+export default createDrawerNavigator
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,6 +12,8 @@ import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
+import { TabNavigator } from 'react-navigation'
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
@@ -19,84 +21,53 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
+      <MapView
+        style={{
+          flex: 1
+        }}
+        initialRegion={{
+          latitude: 33.74825,
+          longitude: -84.3880,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
   };
 }
+
+class Leaderboard extends React.Component {
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text> We all win </Text>
+      </ScrollView>
+    )
+  };
+}
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text> Change Something here </Text>
+      </ScrollView>
+    )
+  };
+}
+
+const HomeScreenTabNavigator = TabNavigator({
+  HomeScreen: {
+    screen: HomeScreen
+  },
+  Leaderboard: {
+    screen: Leaderboard
+  },
+  Settings: {
+    screen: SettingsScreen
+  }
+}, {
+  animationEnabled: true
+})
 
 const styles = StyleSheet.create({
   container: {
