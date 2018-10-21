5cb9d562aa47833fcc3f21c53712710c23d72faa
Ankit <ankit.vishnu23@gmail.com>
2018-10-21 04:26:04 -0400

    Changing Screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,7 +14,15 @@ import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    title: 'Seekr',
+    headerStyle: {
+      backgroundColor: '#fff'
+    },
+    headerTintColor: '#000',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize: '32',
+    },
   };
 
   render() {
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import { Font } from 'expo';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  SafeAreaView
+} from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Seekr',
+    headerStyle: {
+      backgroundColor: '#fff'
+    },
+    headerTintColor: '#000',
+    headerTitleStyle: {
+      fontSize: '32'
+      fontFamily: '../assets/fonts/MANIFESTO.ttf'
+    },
   };
 
   render() {
-    <View>
-    </View>
+    return (
+      <SafeAreaView>
+        <View style = {{height: 150, backgroundColor: 'white',
+          alignItems: 'center', justifyContent: 'center' }}>
+          <Image source = {require('../assets/images/GTLogo.png')} style = {{
+          height: 120, width: 120, borderRadius: 60
+        } }/>
+        </View>
+        <ScrollView style = {{backgroundColor: 'white'}}>
+        </ScrollView>
+      </SafeAreaView>
+    )
   }
 }
